179923dafb471d6389f9dd5c3d4a8f5474d40f66
Mateus Ragazzi <mateusragazzi.b@gmail.com>
2025-05-06 20:38:34 -0400

    chore: improving container commands

--- start-dev.sh
+++ start-dev.sh
@@ -1,9 +1,8 @@
 
 #!/bin/sh
-# Script para iniciar o ambiente de desenvolvimento
 
 docker-compose -f docker-compose-dev.yml down
 
-docker-compose -f docker-compose-dev.yml build
+docker-compose -f docker-compose-dev.yml build --no-cache
 
 docker-compose -f docker-compose-dev.yml up -d
